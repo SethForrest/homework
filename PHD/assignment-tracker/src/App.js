bf11c159d29253c8e9966a9a2082a7b06d933bd0
SethForrest <Seth.Forrest@gmail.com>
2018-06-25 01:37:03 -0700

    making progress

---
+++ PHD/assignment-tracker/src/App.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import 'whatwg-fetch';
+import './App.css';
+
+// const DEBUG = 1;
+
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyMOlSfRbXyRr9Uz' }).base( 'appu8NbYxVqw4YYCp' );
+
+
+
+function Task(title, status, priority, due, id){
+  this.title = title;
+  this.stat = status;
+  this.priority = priority;
+  this.due = due;
+  this.id = id;
+}
+
+function PopulateTable(props){
+  var task_list = [];
+  base('my_table').select({
+      view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+      records.forEach(function(record) {
+        var task = new Task(record.get('Title'), record.get('Status'), record.get('Priority'), record.get('Due Date'), record.get('ID'));
+        // task_list.push( task);
+
+
+        console.log(task);
+        task_list = RenderList(task);
+        // console.log(task_list);
+
+
+      });
+      fetchNextPage();
+  }, function done(err) {
+      if (err) { console.error(err); return "error"; }
+  });
+  // console.log(task_list);
+  return (
+    <ul> {task_list} </ul>
+  );
+}
+
+
+
+function RenderList(task){
+  // console.log("RenderList tasks:", props);
+
+  return(<li> {
+    task.title
+  } </li>);
+}
+
+
+
+// function RenderList(props){
+//   var tasks = props.list;
+//   console.log("props:",props );
+//   console.log("tasks:",tasks );
+//   const listItems=[];
+//   // const listItems = tasks.map(
+//   //   (taskr) => <ListItem key={tasks.id} value={tasks.title} here />
+//   // );
+//   console.log("this",tasks.title);
+//   for (var i=0; i<tasks.length; i++) {
+//     console.log("this",tasks[i]);
+//     // if(tasks.hasOwnProperty(i)) {
+//     //   listItems.push(i);
+//     // }
+//   }
+//   console.log("list:",listItems);
+//   return(<ul> {listItems} </ul>);
+// }
+
+
+// function ListItem(props) {
+// console.log("LI props:",props);
+//   return (<li>{props.value}</li>);
+// }
+
+
+//   function NumberList(props) {
+//     const numbers = props.numbers;
+//     // console.log("props:",props);
+//     // console.log("numbers:",numbers);
+//     const listItems = numbers.map(
+//       (number) => <ListItem key={number.toString()} value={number} />
+//     );
+//     // console.log("Num list:",listItems);
+//     return ( <ul> {listItems} </ul> );
+//   }
+//
+// const numbers = [1,3,4,2,5, 'car'];
+// const tasks = populateTable();
+
+
+// <NumberList numbers={numbers} />
+// <RenderList list={tasks} />
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <div>
+          <PopulateTable />
+        </div>
+      </div>
+    );
+  }
+}
+
+
+export default App;
---
+++ PHD/old/main.js
@@ -0,0 +1,52 @@
+
+// import React from "react";
+
+
+this_value = "test ";
+document.write( this_value );
+
+
+
+// document.write( this_value );
+//
+// const Airtable = require('airtable');
+//
+// Airtable.configure({
+//     // endpointUrl: 'https://api.airtable.com',
+//     apiKey: 'keyMOlSfRbXyRr9Uz'
+// });
+
+document.write( "configured " );
+
+// var base = Airtable.base('appu8NbYxVqw4YYCp');
+//
+//
+//
+// var Airtable = require('airtable');
+// var base = new Airtable({apiKey: 'keyMOlSfRbXyRr9Uz'}).base('appu8NbYxVqw4YYCp');
+//
+//
+//
+// base('Table 1').select({
+//     // Selecting the first 3 records in Grid view:
+//     maxRecords: 3,
+//     view: "Grid view"
+// }).eachPage(function page(records, fetchNextPage) {
+//     // This function (`page`) will get called for each page of records.
+//
+//     records.forEach(function(record) {
+//         console.log('Retrieved', record.get('Title'));
+//     });
+//
+//     // To fetch the next page of records, call `fetchNextPage`.
+//     // If there are more records, `page` will get called again.
+//     // If there are no more records, `done` will get called.
+//     fetchNextPage();
+//
+// }, function done(err) {
+//     if (err) { console.error(err); return; }
+// });
+
+
+
+document.write( " WORKING!!!" );
